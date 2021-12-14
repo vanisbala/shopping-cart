96e1b3a2ac8df1d8f9a3d9fa3caf878602b14727
vani <vanisbala@gmail.com>
2021-12-14 15:54:02 -0800

    Added jsx,css,index.html and images

---
+++ cartdb/cartdb/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '53a26f5490db619e69a236ed719ac058'),
+    },
+  },
+});
---
+++ cartdb/cartdb/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '25fc88c9-1767-4230-bec6-a2f3e763c952'
+};
